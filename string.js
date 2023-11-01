e9c9dd8e5f4e2e75fa0dc129440a1329b3ed2300
Tai Vo <tantaivo74@gmail.com>
2023-11-01 17:32:32 +0700

    isAlphanumeric func

---
+++ string.js
@@ -0,0 +1,11 @@
+function isAlphanumeric(char) {
+  const code = char.charCodeAt(0);
+  if (
+    !(code > 47 && code < 58) && // numeric (0-9)
+    !(code > 64 && code < 91) && // upper alpha (A-Z)
+    !(code > 96 && code < 123)
+  ) {
+    return false;
+  }
+  return true;
+}
